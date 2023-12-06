e684b5f4beebd377659bdf97c04072ced6f49f46
Ben Noble <ben.noble@madetech.com>
2023-12-06 14:29:49 +0000

    Can check equality between all units

--- quantity.js
+++ quantity.js
@@ -1,3 +1,22 @@
+class Unit{
+    constructor(ratioToBaseUnit = 1, baseUnit){
+        this._ratioToBaseUnit = ratioToBaseUnit;
+        this._baseUnit = baseUnit;
+    }
+
+    s(amount){
+        return new Quantity(amount, this)
+    }
+
+    amountInBaseUnit(amount){
+        if (this._baseUnit == undefined) {
+            return amount * this._ratioToBaseUnit;
+        }
+
+        return this._baseUnit.amountInBaseUnit(amount*this._ratioToBaseUnit);
+    }
+}
+
 class Quantity{
     constructor(amount, unit){
         this._amount = amount;
@@ -5,12 +24,16 @@ class Quantity{
     }
 
     equals(other){
-        return this._amount == other._amount && this._unit == other._unit;
+        return this._unit.amountInBaseUnit(this._amount) == other._unit.amountInBaseUnit(other._amount)
     }
 }
 
-const TEASPOON = new Object();
-const TABLESPOON = new Object();
-TEASPOON.s = (amount) => new Quantity(amount, TEASPOON);
+const TEASPOON = new Unit();
+const TABLESPOON = new Unit(3, TEASPOON);
+const OUNCE = new Unit(2, TABLESPOON);
+const CUP = new Unit(8, OUNCE);
+const PINT = new Unit(2, CUP);
+const QUART = new Unit(2, PINT);
+const GALLON = new Unit(4, QUART);
 
-module.exports = {Quantity, TEASPOON, TABLESPOON}
+module.exports = {Quantity, TEASPOON, TABLESPOON, OUNCE, CUP, PINT, QUART, GALLON}
--- quantity.test.js
+++ quantity.test.js
@@ -1,4 +1,4 @@
-const {Quantity, TEASPOON, TABLESPOON, OUNCE} = require("./quantity");
+const {Quantity, TEASPOON, TABLESPOON, OUNCE, CUP, PINT, QUART, GALLON} = require("./quantity");
 
 describe ('Quantity objects....', () => {
     test('are equal when quantity and unity match', () => {
@@ -8,6 +8,7 @@ describe ('Quantity objects....', () => {
         expect(new Quantity(1, TEASPOON).equals(new Quantity(1, TABLESPOON))).toBe(false);
         expect(new Quantity(1, TABLESPOON).equals(new Quantity(1, TABLESPOON))).toBe(true);
 
+        expect(new Quantity(1, TEASPOON).equals(new Quantity(1, OUNCE))).toBe(false);
         expect(new Quantity(1, TABLESPOON).equals(new Quantity(1, OUNCE))).toBe(false);
     })
 
@@ -15,5 +16,26 @@ describe ('Quantity objects....', () => {
         expect(TEASPOON.s(3).equals(TEASPOON.s(3))).toBe(true);
         expect(TEASPOON.s(3).equals(TEASPOON.s(6))).toBe(false);
         expect(TEASPOON.s(6).equals(TEASPOON.s(3))).toBe(false);
+    })
+
+    test ('teaspoons and tablespoons can be equal if at the correct ratio', () => {
+        expect(new Quantity(1, TABLESPOON).equals(new Quantity(3, TEASPOON))).toBe(true);
+        expect(new Quantity(6, TEASPOON).equals(new Quantity(2, TABLESPOON))).toBe(true);
+    })
+
+    test ('Quantities can be equal if if at the correct ratio', () => {
+        expect(new Quantity(1, OUNCE).equals(new Quantity(2, TABLESPOON))).toBe(true);
+        expect(new Quantity(4, TABLESPOON).equals(new Quantity(2, OUNCE))).toBe(true);
+
+        expect(new Quantity(12, TEASPOON).equals(new Quantity(2, OUNCE))).toBe(true);
+        expect(OUNCE.s(3).equals(TEASPOON.s(18))).toBe(true);
+
+        expect(CUP.s(1).equals(OUNCE.s(8))).toBe(true);
+        expect(PINT.s(1).equals(CUP.s(2))).toBe(true);
+        expect(QUART.s(1).equals(PINT.s(2))).toBe(true);
+        expect(GALLON.s(1).equals(QUART.s(4))).toBe(true);
+
+        expect(GALLON.s(1).equals(TEASPOON.s(4*2*2*8*2*3))).toBe(true);
+        expect(TEASPOON.s(4*2*2*8*2*3).equals(GALLON.s(1))).toBe(true);
     })
 })
